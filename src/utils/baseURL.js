347cfc053f090c5e32c7b7317be5445867995e50
yuankai718 <2324764882@qq.com>
2021-05-16 14:43:32 +0800

    bastUrl修改

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
-// const baseUrl = "http://47.103.201.247:8181"
-const baseUrl = "http://localhost:8181"
+const baseUrl = "http://47.103.201.247:8181"
+// const baseUrl = "http://localhost:8181"
 export default baseUrl
 
 /**
